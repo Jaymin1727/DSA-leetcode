2a144b415f098da89304db769651b4f24bf27a7a
Jaymin1727 <jayminhalpati548@gmail.com>
2026-07-10 16:02:57 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (37.30%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -4,8 +4,10 @@ class Solution {
        int end=s.length-1;
        while(start<end){
             char temp=s[start];
-            s[start++]=s[end];
-            s[end--]=temp;
+            s[start]=s[end];
+            s[end]=temp;
+            start++;
+            end--;
        }
     }
 }
