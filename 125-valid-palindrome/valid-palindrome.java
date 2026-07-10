eb627ea38f9c4e754d4386e7ac43e1f46b2719ae
Jaymin1727 <jayminhalpati548@gmail.com>
2026-07-10 16:21:05 +0530

    Time: 14 ms (31.77%) | Memory: 46.5 MB (24.30%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null)        
+        {
+            return true;
+        }
+
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = cleaned.length() - 1;
+        
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+}
