2836c755dd8e1e4eaae6b1eb0a8ee0fafc01822c
Jaymin1727 <jayminhalpati548@gmail.com>
2026-07-20 21:37:07 +0530

    Time: 3 ms (58.15%) | Memory: 47.4 MB (10.95%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -5,12 +5,11 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
             int current=target-nums[i];
-
             if(map.containsKey(current)){
                 return new int[]{map.get(current),i};
             }
             map.put(nums[i],i);
         }
-        return new int[]{};
+        return new int[]{};  
     }
 }
